c0b4ef7b620175bb413faa5e5e7291c585901129
김연진 <kyj0719@gmail.com>
2025-08-11 15:12:15 +0900

    🐛fix: import 에러 수정

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,5 +1,5 @@
 // Node 전용 공개 엔트리
-export { applyThemes } from './node/applyThemes.ts';
+export { applyThemes } from './node/applyThemes';
 
 // 타입도 필요하면 re-export
 export type {} from /* ThemeGenInput 등 필요하면 여기서 */ './node/applyThemes.ts';
